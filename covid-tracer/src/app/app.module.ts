24e8966b92426748d96320e99017167b3919824c
chinmayee212 <cvwaingankar@gmail.com>
2021-03-13 15:50:01 +0530

    New Component

--- covid-tracer/src/app/app.module.ts
+++ covid-tracer/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LayoutComponent } from './layout/layout.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { TopbarComponent } from './layout/topbar/topbar.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
     LayoutComponent,
     FooterComponent,
     TopbarComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
