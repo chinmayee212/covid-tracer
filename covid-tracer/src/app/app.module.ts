707317126813f04445543f960b55c46f0a74b8fb
chinmayee212 <cvwaingankar@gmail.com>
2021-03-13 15:40:08 +0530

    Layouts Services

--- covid-tracer/src/app/app.module.ts
+++ covid-tracer/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    FooterComponent,
+    TopbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
