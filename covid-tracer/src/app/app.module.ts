e2927bf98758dea2f03ade3d7bc77e314f267a2f
chinmayee212 <cvwaingankar@gmail.com>
2021-03-13 17:42:06 +0530

    Internalition concept

--- covid-tracer/src/app/app.module.ts
+++ covid-tracer/src/app/app.module.ts
@@ -9,7 +9,15 @@ import { TopbarComponent } from './layout/topbar/topbar.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
+import { TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// tslint:disable-next-line: typedef
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +30,21 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  exports: [
+    LayoutComponent,
+    FooterComponent,
+    TopbarComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- covid-tracer/src/app/core/services/getdata.service.ts
+++ covid-tracer/src/app/core/services/getdata.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class GetdataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private host = 'https://disease.sh/v3/covid-19/all';
+
+
 }
